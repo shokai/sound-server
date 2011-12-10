b1a5bc63de1fd828cfb86ce8c86b1c0d9185a30c
Sho Hashimoto <hashimoto@shokai.org>
2011-12-10 19:36:03 +0900

    add sound permalink

--- controllers/main.rb
+++ controllers/main.rb
@@ -10,3 +10,15 @@ get '/:name/upload' do
   @name = params[:name]
   haml :sound_upload
 end
+
+get '/:name/*' do
+  @name = params[:name]
+  @fname = params[:splat].first.first
+  if @fname.to_s.size < 1 or !File.exists?("#{file_dir(@name)}/#{@fname}")
+    status 404
+    @mes = 'not found'
+  else
+    content_type = 'text/html'
+    haml :sound
+  end
+end
