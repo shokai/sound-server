14c945f4c1e6943c2e0d5e19c5965afb6de7de53
Sho Hashimoto <hashimoto@shokai.org>
2011-12-13 02:04:46 +0900

    input text in speechinput

--- public/js/sound_upload.js
+++ public/js/sound_upload.js
@@ -1,27 +1,27 @@
 $(
     function(){
-        $('#speech')[0].addEventListener(
-            'webkitspeechchange',
-            function(){
-                var speech = $('#speech').val();
-                $.ajax(
-                    {
-                        url : app_root+'/'+user+'/upload_text',
-                        data : {
-                            text : speech, no_redirect : true
-                        },
-                        success : function(res){
-                            console.log(res);
-                            location.href = res;
-                        },
-                        error : function(e){
-                            console.log(e);
-                        },
-                        type : 'POST',
-                        dataType : 'text'
-                    }
-                );
-            }
-        );
+        $('#btn_speech').click(send_speech);
+        $('#speech')[0].addEventListener('webkitspeechchange', send_speech);
     }
 );
+
+var send_speech = function(){
+    var speech = $('#speech').val();
+    $.ajax(
+        {
+            url : app_root+'/'+user+'/upload_text',
+            data : {
+                text : speech, no_redirect : true
+            },
+            success : function(res){
+                console.log(res);
+                location.href = res;
+            },
+            error : function(e){
+                console.log(e);
+            },
+            type : 'POST',
+            dataType : 'text'
+        }
+    );    
+};
